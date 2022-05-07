df1e21f8c4c78b98607aff3eb02df3f0cb6290d1
MrStrelan <90510167+MrStrelan@users.noreply.github.com>
2022-05-07 17:42:48 +0200

    Everything added to table of bookings

--- src/main/java/com/nordic_motorhome_project/controller/BookingController.java
+++ src/main/java/com/nordic_motorhome_project/controller/BookingController.java
@@ -1,8 +1,10 @@
 package com.nordic_motorhome_project.controller;
 
 import com.nordic_motorhome_project.model.Booking;
+import com.nordic_motorhome_project.model.Customer;
 import com.nordic_motorhome_project.model.MotorhomeModel;
 import com.nordic_motorhome_project.repository.BookingRepository;
+import com.nordic_motorhome_project.service.CustomerService;
 import com.nordic_motorhome_project.service.MotorhomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,15 +23,20 @@ public class BookingController {
     private BookingRepository bookingRepository;
     @Autowired
     private MotorhomeService motorhomeService;
+    @Autowired
+    private CustomerService customerService;
 
     @GetMapping("/booking.html")
     public String booking(Model model){
         //bookings
         List<Booking> bookings = bookingRepository.getBookings();
         model.addAttribute("bookings", bookings);
-        //all motorhomes
+        //motorhomes
         List<MotorhomeModel> motorhomesList = motorhomeService.getMotorhomes();
         model.addAttribute("motorhomes", motorhomesList);
+        //customers
+        List<Customer> customerList = customerService.getCustomers();
+        model.addAttribute("customers", customerList);
         return "home/booking";
     }
 }
