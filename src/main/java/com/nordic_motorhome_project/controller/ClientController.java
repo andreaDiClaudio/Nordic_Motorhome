903f18bfdfa827493b5d5f9b668c34f16c3066f4
andreaDiClaudio <andreadicla@gmail.com>
2022-05-07 17:39:29 +0200

    Extra styling for index and motorhome

--- src/main/java/com/nordic_motorhome_project/controller/ClientController.java
+++ src/main/java/com/nordic_motorhome_project/controller/ClientController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BookingController {
+public class ClientController {
 
-    @GetMapping("/booking")
-    public String booking(){
-        return "home/booking";
+    @GetMapping("/client")
+    public String client(){
+        return "home/client";
     }
 }
