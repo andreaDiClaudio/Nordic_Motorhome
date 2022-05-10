49fd1f4556f14532aa182f87675b472ad7947773
MrStrelan <90510167+MrStrelan@users.noreply.github.com>
2022-05-10 10:13:11 +0200

    Blueprint of the page

--- src/main/java/com/nordic_motorhome_project/controller/BookingController.java
+++ src/main/java/com/nordic_motorhome_project/controller/BookingController.java
@@ -1,13 +1,39 @@
 package com.nordic_motorhome_project.controller;
 
+import com.nordic_motorhome_project.model.Booking;
+import com.nordic_motorhome_project.model.Customer;
+import com.nordic_motorhome_project.model.MotorhomeModel;
+import com.nordic_motorhome_project.repository.BookingRepository;
+import com.nordic_motorhome_project.service.CustomerService;
+import com.nordic_motorhome_project.service.MotorhomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class BookingController {
 
+    @Autowired
+    private BookingRepository bookingRepository;
+    @Autowired
+    private MotorhomeService motorhomeService;
+    @Autowired
+    private CustomerService customerService;
+
     @GetMapping("/booking")
-    public String booking(){
+    public String booking(Model model){
+        //bookings
+        List<Booking> bookings = bookingRepository.getBookings();
+        model.addAttribute("bookings", bookings);
+        //motorhomes
+        List<MotorhomeModel> motorhomesList = motorhomeService.getMotorhomes();
+        model.addAttribute("motorhomes", motorhomesList);
+        //customers
+        List<Customer> customerList = customerService.getCustomers();
+        model.addAttribute("customers", customerList);
         return "home/booking";
     }
 }
