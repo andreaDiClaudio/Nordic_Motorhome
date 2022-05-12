3bc271ec54eab73ea4203e51ebd31dcb232bd2c2
andreaDiClaudio <andreadicla@gmail.com>
2022-05-12 17:57:04 +0200

    added mapping for errors

--- src/main/java/com/nordic_motorhome_project/controller/IndexController.java
+++ src/main/java/com/nordic_motorhome_project/controller/IndexController.java
@@ -16,18 +16,14 @@ public class IndexController {
     //@GetMapping annotation is for mapping http GET requests onto specific handler methods.
     //Specifically, @GetMapping is a composed annotation that acts as shortcut for @RequestMapping(method = RequestMethod.GET)
     // When the client asks for "/" the method will return "home/index" which is the html file inside resources>templates.home>index.html
-    //This mapping is for "each" first time you enter the web page.
     @GetMapping("/")
     public String index(){
         return "home/index";
     }
 
-    /*
-    //This mapping is for when you click any <a> tag with href=index.html.
-    @GetMapping("/index.html")
-    public String goBack(){
-        return "home/index";
+    @GetMapping("/error")
+    public String error(){
+        return "/error";
     }
 
-     */
 }
