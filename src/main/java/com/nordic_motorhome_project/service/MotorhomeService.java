b29aa5fc9e569279e512b0edbd07332cf4a20d7e
andreaDiClaudio <andreadicla@gmail.com>
2022-05-12 17:06:07 +0200

    Search bar added

--- src/main/java/com/nordic_motorhome_project/service/MotorhomeService.java
+++ src/main/java/com/nordic_motorhome_project/service/MotorhomeService.java
@@ -5,6 +5,7 @@ import com.nordic_motorhome_project.repository.MotorhomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //@Service annotation is used with classes that proved business functionalities.
@@ -25,5 +26,11 @@ public class MotorhomeService {
     public void deleteMotorhome(String license_plate){motorhomeRepository.deleteMotorhome(license_plate);}
 
     public List<MotorhomeModel> getMotorhomeTypeDesc(){ return motorhomeRepository.getMotorhomeTypeDesc();}
+
+    public ArrayList<MotorhomeModel> searchMotorhome(String license_plate){
+        ArrayList<MotorhomeModel> list=new ArrayList<>();
+        list.add(findMotorhomeByLicensePlate(license_plate));
+        return list;
+    }
 }
 
