bc3f31189929173bd59ca79902dd0ecebc2a0c25
MrStrelan <90510167+MrStrelan@users.noreply.github.com>
2022-05-20 10:49:44 +0200

    MERGE INTO MAIN

--- src/main/resources/js/booking.js
+++
@@ -1,6 +0,0 @@
-const changeSelected = (e) => {
-    const $select = document.querySelector('#mySelect');
-    $select.value = 'steve'
-};
-
-document.querySelector('.changeSelected').addEventListener('click', changeSelected);
